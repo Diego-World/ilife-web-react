4a422082289c02930912f0f1977853834e3dc8bc
Abner Mendes <93213670+ClaushSouza@users.noreply.github.com>
2024-06-06 15:34:36 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
           <Route exact path="/" element={<LoginForm/ >}/>
           <Route path="/register" element={<RegisterForm/ >}/>
           <Route path="/register" element={<RegisterForm/ >}/>
+          <Route path="/register" element={<RegisterForm/ >}/>
         </Routes>
     </div>
   </Router>
